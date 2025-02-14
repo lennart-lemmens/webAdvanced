deb133f480318a2fb35aa3d7d101cd1031b1c36f
Lennart Lemmens <lennartlemmens@gmail.com>
2025-02-14 16:03:58 +0100

    Finished M0H1

--- Module0/H1/script.js
+++ Module0/H1/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+let themaKnop = document.getElementById('themaKnop');
+let body = document.getElementsByTagName('body');
+let a = document.getElementsByTagName('a');
+
+themaKnop.addEventListener('click', () => {
+    body[0].classList.toggle('dark')
+    
+    for (let i = 0; i < a.length; i++)
+    {
+        a[i].classList.toggle('dark');
+    }
+
+    themaKnop.innerHTML == 'Donker thema' ? themaKnop.innerHTML = 'Licht thema' : themaKnop.innerHTML = 'Donker thema';
+}, false)
