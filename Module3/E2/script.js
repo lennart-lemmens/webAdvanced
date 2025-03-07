69db8539e3850429ac19ddc4ccf761f1239580e0
Lennart Lemmens <lennartlemmens@gmail.com>
2025-03-07 19:47:50 +0100

    Added timestamp.

--- Module3/E2/script.js
+++ Module3/E2/script.js
@@ -2,14 +2,16 @@
 
 const output = document.getElementById('output');
 
-const maakBestelling = (drank = 'cola', snack = 'chips') => {
-    return {drank: drank, snack: snack};
+const maakBestelling = (drink = 'cola', snack = 'chips') => {
+    let date = new Date();
+    let timestamp = date.toLocaleString("nl-BE");
+    return {drink: drink, snack: snack, timestamp: timestamp};
 }
 
 let bestelling1 = maakBestelling();
 let bestelling2 = maakBestelling('fanta');
 let bestelling3 = maakBestelling('sprite', 'nootjes');
 
-output.innerHTML += `Bestelling 1: ${bestelling1.drank} en ${bestelling1.snack}</br>`;
-output.innerHTML += `Bestelling 2: ${bestelling2.drank} en ${bestelling2.snack}</br>`;
-output.innerHTML += `Bestelling 3: ${bestelling3.drank} en ${bestelling3.snack}`;
+output.innerHTML += `<tr><td>${bestelling1.timestamp}: </td><td>${bestelling1.drink} en ${bestelling1.snack}</td></tr>`;
+output.innerHTML += `<tr><td>${bestelling2.timestamp}: </td><td>${bestelling2.drink} en ${bestelling2.snack}</td></tr>`;
+output.innerHTML += `<tr><td>${bestelling3.timestamp}: </td><td>${bestelling3.drink} en ${bestelling3.snack}</td></tr>`;
