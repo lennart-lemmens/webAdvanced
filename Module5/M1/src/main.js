71576492c56a3014ba0adae6574290d7b1e38abb
Lennart Lemmens <lennartlemmens@gmail.com>
2025-04-18 18:06:10 +0200

    Added voegAfbeeldingToe() function to streamline code.

--- Module5/M1/src/main.js
+++ Module5/M1/src/main.js
@@ -19,23 +19,24 @@ const laadAfbeelding = url => {
   });
 };
 
+async function voegAfbeeldingToe(img, imgNumber, totalImgNumber) {
+  try {
+    const image = await laadAfbeelding(img);
+    gallery.appendChild(image);
+    let progress = (imgNumber / totalImgNumber) * 100;
+    progressBar.innerHTML = `${progress.toFixed(0)}%`;
+    progressBar.style.width = `${progress.toFixed(0)}%`;
+  } catch (error) {
+    console.error("Er ging iets mis: ", error);
+  }
+};
+
 async function laadAlleAfbeeldingen() {
   gallery.innerHTML = "";
   try {
-    const img1 = await laadAfbeelding("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/892970/capsule_616x353.jpg?t=1738051073");
-    gallery.appendChild(img1);
-    progressBar.innerHTML = "33%";
-    progressBar.style.width = "33%";
-
-    const img2 = await laadAfbeelding("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/553850/header.jpg?t=1741137570");
-    gallery.appendChild(img2);
-    progressBar.innerHTML = "67%";
-    progressBar.style.width = "67%";
-
-    const img3 = await laadAfbeelding("https://store-images.s-microsoft.com/image/apps.50670.13727851868390641.c9cc5f66-aff8-406c-af6b-440838730be0.d205e025-5444-4eb1-ae46-571ae6895928?q=90&w=480&h=270");
-    gallery.appendChild(img3);
-    progressBar.innerHTML = "100%";
-    progressBar.style.width = "100%";
+    await voegAfbeeldingToe("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/892970/capsule_616x353.jpg?t=1738051073", 1, 3);
+    await voegAfbeeldingToe("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/553850/header.jpg?t=1741137570", 2, 3);
+    await voegAfbeeldingToe("https://store-images.s-microsoft.com/image/apps.50670.13727851868390641.c9cc5f66-aff8-406c-af6b-440838730be0.d205e025-5444-4eb1-ae46-571ae6895928?q=90&w=480&h=270", 3, 3);
 
     console.log("Alle afbeeldingen zijn geladen.");
   } catch (error) {
